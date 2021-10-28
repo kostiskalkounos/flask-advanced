4c7938f23d3b378b7f08eaff2c20c4f6896934d7
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-28 16:06:25 +0200

    update Store

--- resources/store.py
+++ resources/store.py
@@ -1,6 +1,8 @@
-from models.store import StoreModel
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from flask_jwt import jwt_required
+
+from models.store import StoreModel
+from schemas.store import StoreSchema
 
 BLANK_ERROR = "'{}' cannot be blank."
 ERROR_INSERTING = "An error occurred while creating the store."
@@ -8,26 +10,17 @@ NAME_ALREADY_EXISTS = "A Store with name '{}' already exists."
 STORE_DELETED = "Store deleted."
 STORE_NOT_FOUND = "Store not found."
 
+store_schema = StoreSchema()
+store_list_schema = StoreSchema(many=True)
+
+
 class Store(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                         type=float,
-                         required=True,
-                         help=BLANK_ERROR.format("price")
-    )
-
-    parser.add_argument('store_id',
-                         type=int,
-                         required=True,
-                         help=BLANK_ERROR.format("store_id")
-    )
-
     @classmethod
     @jwt_required()
     def get(cls, name: str):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
+            return store_schema.dump(store)
         return {'message': STORE_NOT_FOUND}, 404 # this is a tuple
 
 
@@ -35,14 +28,14 @@ class Store(Resource):
         if StoreModel.find_by_name(name):
             return {'message':NAME_ALREADY_EXISTS.format(name)}, 400
 
-        store = StoreModel(name)
+        store = StoreModel(name=name)
 
         try:
             store.save_to_db()
         except:
             return {'message': ERROR_INSERTING}, 500
 
-        return store.json(), 201
+        return store_schema.dump(store), 201
 
     @classmethod
     @jwt_required()
@@ -57,5 +50,4 @@ class Store(Resource):
 class StoreList(Resource):
     @classmethod
     def get(cls):
-        # return {'stores': list(map(lambda x: x.json(), StoreModel.find_all()))}
-        return {'stores': [store.json() for store in StoreModel.find_all()]}
+        return {'stores': store_list_schema.dump(StoreModel.find_all())}
