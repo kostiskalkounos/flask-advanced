86f703b7b3322cd37d50bbd02681c90242b42dde
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-30 12:09:28 +0200

    tell users they are active

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, make_response, render_template
 from flask_restful import Resource
 from flask_jwt_extended import (
         create_access_token,
@@ -101,5 +101,6 @@ class UserConfirm(Resource):
 
         user.activated = True
         user.save_to_db()
-        return {"message": USER_CONFIRMED}, 200
+        headers = {"Content-Type": "text/html"}
+        return make_response(render_template("confirmation_page.html", email=user.email), 200, headers)
 
