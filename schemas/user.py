0fe5fb1a7d741b845d0bff4238afe983c8121595
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-31 16:34:24 +0100

    add confirmation to the user schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,4 +1,5 @@
 from ma import ma
+from marshmallow import pre_dump
 from models.user import UserModel
 
 
@@ -6,4 +7,9 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
         load_only = ("password",) # it's a tuple
-        dump_only = ("id", "activated") # not required in this scenario
+        dump_only = ("id", "confirmation")
+
+    @pre_dump
+    def _pre_dump(self, user: UserModel, **kwargs):
+        user.confirmation = [user.most_recent_confirmation]
+        return user
